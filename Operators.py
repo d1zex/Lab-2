63bc1e33a844db011aede915a15a076cc42e9e51
d1zex <ospanaren@mail.ru>
2025-01-27 15:56:39 +0500

    Python Comparison Operators

--- Operators.py
+++ Operators.py
@@ -71,3 +71,36 @@ print(x)
 
 print(x := 3)
 
+#Python Comparison Operators
+#Comparison operators are used to compare two values:
+x = 5
+y = 3
+print(x == y)
+# returns False because 5 is not equal to 3
+
+x = 5
+y = 3
+print(x != y)
+# returns True because 5 is not equal to 3
+
+x = 5
+y = 3
+print(x > y)
+# returns True because 5 is greater than 3
+
+x = 5
+y = 3
+print(x < y)
+# returns False because 5 is not less than 3
+
+x = 5
+y = 3
+print(x >= y)
+# returns True because five is greater, or equal, to 3
+
+x = 5
+y = 3
+print(x <= y)
+# returns False because 5 is neither less than or equal to 3
+
+
