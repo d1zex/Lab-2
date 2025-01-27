647fdee04b698b26f3cb15b99d54e68e65c0e4c6
d1zex <ospanaren@mail.ru>
2025-01-27 16:26:35 +0500

    bla bla bla

---
+++ Add List Items.py
@@ -0,0 +1,11 @@
+#Python - Add List Items
+
+#Using the append() method to append an item:
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+#Insert an item as the second position:
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
