fc8c65c67b59f94710635690e79eb5e12ef61947
d1zex <ospanaren@mail.ru>
2025-01-27 16:29:21 +0500

    last commit

--- Add List Items.py
+++ Add List Items.py
@@ -9,3 +9,15 @@ print(thislist)
 thislist = ["apple", "banana", "cherry"]
 thislist.insert(1, "orange")
 print(thislist)
+
+#Add the elements of tropical to thislist:
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+#Add elements of a tuple to a list:
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
