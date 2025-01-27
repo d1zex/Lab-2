1c2b7d48a4dcbd925597184f8a2c61fe2707cd98
d1zex <ospanaren@mail.ru>
2025-01-27 15:53:17 +0500

    PY arithmetic and assignment operators

---
+++ Operators.py
@@ -0,0 +1,73 @@
+#Operators are used to perform operations on variables and values.
+#In the example below, we use the + operator to add together two values:
+print(10 + 5) 
+
+x = 5
+y = 2
+print(x % y)
+
+
+x = 2
+y = 5
+print(x ** y) #same as 2*2*2*2*2
+
+x = 15
+y = 2
+print(x // y)
+#the floor division // rounds the result down to the nearest whole number
+
+#Assignment operators are used to assign values to variables:
+x = 5
+print(x)
+
+x = 5
+x += 3
+print(x)
+
+x = 5
+x -= 3
+print(x)
+
+
+x = 5
+x *= 3
+print(x)
+
+x = 5
+x /= 3
+print(x)
+
+x = 5
+x%=3
+print(x)
+
+x = 5
+x//=3
+print(x)
+
+x = 5
+x **= 3
+print(x)
+
+x = 5
+x &= 3
+print(x)
+
+x = 5
+x |= 3
+print(x)
+
+x = 5
+x ^= 3
+print(x)
+
+x = 5
+x >>= 3
+print(x)
+
+x = 5
+x <<= 3
+print(x)
+
+print(x := 3)
+
