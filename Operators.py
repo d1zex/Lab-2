655676fe17aa923fc7a999adb0a729df85382c0d
d1zex <ospanaren@mail.ru>
2025-01-27 16:07:55 +0500

    PY Bitwise opearators and Operator Precedence

--- Operators.py
+++ Operators.py
@@ -155,3 +155,161 @@ x = ["apple", "banana"]
 print("pineapple" not in x)
 # returns True because a sequence with the value "pineapple" is not in the list
 
+
+#Python Bitwise Operators
+#Bitwise operators are used to compare (binary) numbers:
+
+print(6 & 3)
+"""
+The & operator compares each bit and set it to 1 if both are 1, otherwise it is set to 0:
+6 = 0000000000000110
+3 = 0000000000000011
+--------------------
+2 = 0000000000000010
+====================
+Decimal numbers and their binary values:
+0 = 0000000000000000
+1 = 0000000000000001
+2 = 0000000000000010
+3 = 0000000000000011
+4 = 0000000000000100
+5 = 0000000000000101
+6 = 0000000000000110
+7 = 0000000000000111
+"""
+
+print(6 | 3)
+"""
+The | operator compares each bit and set it to 1 if one or both is 1, otherwise it is set to 0:
+
+6 = 0000000000000110
+3 = 0000000000000011
+--------------------
+7 = 0000000000000111
+====================
+
+Decimal numbers and their binary values:
+0 = 0000000000000000
+1 = 0000000000000001
+2 = 0000000000000010
+3 = 0000000000000011
+4 = 0000000000000100
+5 = 0000000000000101
+6 = 0000000000000110
+7 = 0000000000000111
+"""
+
+
+print(6 ^ 3)
+"""
+The ^ operator compares each bit and set it to 1 if only one is 1, otherwise (if both are 1 or both are 0) it is set to 0:
+
+6 = 0000000000000110
+3 = 0000000000000011
+--------------------
+5 = 0000000000000101
+====================
+
+Decimal numbers and their binary values:
+0 = 0000000000000000
+1 = 0000000000000001
+2 = 0000000000000010
+3 = 0000000000000011
+4 = 0000000000000100
+5 = 0000000000000101
+6 = 0000000000000110
+7 = 0000000000000111
+"""
+
+print(~3)
+"""
+The ~ operator inverts each bit (0 becomes 1 and 1 becomes 0).
+
+Inverted 3 becomes -4:
+ 3 = 0000000000000011
+-4 = 1111111111111100
+
+Decimal numbers and their binary values:
+ 4 = 0000000000000100
+ 3 = 0000000000000011
+ 2 = 0000000000000010
+ 1 = 0000000000000001
+ 0 = 0000000000000000
+-1 = 1111111111111111
+-2 = 1111111111111110
+-3 = 1111111111111101
+-4 = 1111111111111100
+"""
+
+print(3 << 2)
+"""
+The << operator inserts the specified number of 0's (in this case 2) from the right and let the same amount of leftmost bits fall off:
+
+If you push 00 in from the left:
+ 3 = 0000000000000011
+becomes
+12 = 0000000000001100
+
+Decimal numbers and their binary values:
+ 0 = 0000000000000000
+ 1 = 0000000000000001
+ 2 = 0000000000000010
+ 3 = 0000000000000011
+ 4 = 0000000000000100
+ 5 = 0000000000000101
+ 6 = 0000000000000110
+ 7 = 0000000000000111
+ 8 = 0000000000001000
+ 9 = 0000000000001001
+10 = 0000000000001010
+11 = 0000000000001011
+12 = 0000000000001100
+"""
+
+print(8 >> 2)
+"""
+The >> operator moves each bit the specified number of times to the right. Empty holes at the left are filled with 0's.
+
+If you move each bit 2 times to the right, 8 becomes 2:
+ 8 = 0000000000001000
+becomes
+ 2 = 0000000000000010
+
+Decimal numbers and their binary values:
+ 0 = 0000000000000000
+ 1 = 0000000000000001
+ 2 = 0000000000000010
+ 3 = 0000000000000011
+ 4 = 0000000000000100
+ 5 = 0000000000000101
+ 6 = 0000000000000110
+ 7 = 0000000000000111
+ 8 = 0000000000001000
+ 9 = 0000000000001001
+10 = 0000000000001010
+11 = 0000000000001011
+12 = 0000000000001100
+"""
+
+#Operator Precedence
+#Operator precedence describes the order in which operations are performed.
+print((6 + 3) - (6 + 3))
+"""
+Parenthesis have the highest precedence, and need to be evaluated first.
+The calculation above reads 9 - 9 = 0
+"""
+
+#Multiplication * has higher precedence than addition +, and therefor multiplications are evaluated before additions:
+print(100 + 5 * 3)
+"""
+The calculation above reads 100 + 15 = 115
+"""
+
+#Addition + and subtraction - has the same precedence, and therefor we evaluate the expression from left to right:
+print(5 + 4 - 7 + 3)
+"""
+The calculation above reads:
+5 + 4 = 9
+9 - 7 = 2
+2 + 3 = 5
+"""
